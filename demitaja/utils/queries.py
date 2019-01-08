d58cf5fc43fd58aadbbcbe9fdfb0d42e683895c1
Michal <pollux.pw@gmail.com>
2019-01-08 21:33:24 +0100

    Add display basic-summary-of-job-market-data functionality

---
+++ demitaja/utils/queries.py
@@ -0,0 +1,135 @@
+"""SQL queries."""
+
+import sqlalchemy
+
+
+# Get 10 most in-demand technologies
+techs = sqlalchemy.sql.text(
+    "SELECT technologies.name, COUNT(*) AS count "
+    "FROM postings_techs_must "
+    "JOIN technologies "
+    "ON postings_techs_must.must_id=technologies.id "
+    "GROUP BY technologies.name "
+    "ORDER BY count DESC "
+    "LIMIT 10"
+)
+
+
+# Get 10 cities with most postings
+cities = sqlalchemy.sql.text(
+    "SELECT cities.name, COUNT(*) AS count "
+    "FROM postings_cities "
+    "JOIN cities "
+    "ON postings_cities.city_id=cities.id "
+    "GROUP BY cities.name "
+    "ORDER BY count DESC "
+    "LIMIT 10"
+)
+
+# Get number of job postings in a given city
+city = sqlalchemy.sql.text(
+    "SELECT cities.name, COUNT(*) "
+    "FROM postings_cities "
+    "JOIN cities "
+    "ON postings_cities.city_id=cities.id "
+    "WHERE cities.name=:city "
+    "GROUP BY cities.name"
+)
+
+# Get demand for a given technology
+tech = sqlalchemy.sql.text(
+    "SELECT technologies.name, COUNT(*) "
+    "FROM postings_techs_must "
+    "JOIN technologies "
+    "ON postings_techs_must.must_id=technologies.id "
+    "WHERE technologies.name=:tech "
+    "GROUP BY technologies.name"
+)
+
+# Get 5 cities with most postings requiring a given technology
+cities_tech = sqlalchemy.sql.text(
+    "SELECT cities.name, technologies.name, COUNT(*) AS count "
+    "FROM postings_cities "
+    "JOIN cities "
+        "ON postings_cities.city_id=cities.id "
+    "JOIN postings_techs_must "
+        "ON postings_techs_must.posting_id=postings_cities.posting_id "
+    "JOIN technologies "
+        "ON postings_techs_must.must_id=technologies.id "
+    "WHERE technologies.name=:tech "
+    "GROUP BY cities.name "
+    "ORDER BY count DESC "
+    "LIMIT 5"
+)
+
+# Get 5 most in-demand technologies in a given city
+techs_city = sqlalchemy.sql.text(
+    "SELECT technologies.name, cities.name, COUNT(*) AS count "
+    "FROM postings_cities "
+    "JOIN cities "
+        "ON postings_cities.city_id=cities.id "
+    "JOIN postings_techs_must "
+        "ON postings_techs_must.posting_id=postings_cities.posting_id "
+    "JOIN technologies "
+        "ON postings_techs_must.must_id=technologies.id "
+    "WHERE cities.name=:city "
+    "GROUP BY technologies.name "
+    "ORDER BY count DESC "
+    "LIMIT 5"
+)
+
+# Get demand for a given technology in a given city
+tech_city = sqlalchemy.sql.text(
+    "SELECT technologies.name, cities.name, COUNT(*) AS count "
+    "FROM postings_cities "
+    "JOIN cities "
+        "ON postings_cities.city_id=cities.id "
+    "JOIN postings_techs_must "
+        "ON postings_techs_must.posting_id=postings_cities.posting_id "
+    "JOIN technologies "
+        "ON postings_techs_must.must_id=technologies.id "
+    "WHERE cities.name=:city "
+        "AND technologies.name=:tech "
+    "GROUP BY technologies.name "
+    "ORDER BY count DESC "
+    "LIMIT 5"
+)
+
+# Get 5 technologies most often accompanying a given technology in postings
+techs_tech = sqlalchemy.sql.text(
+    "SELECT t1.name, t2.name, COUNT(*) AS count "
+    "FROM technologies t1 "
+    "JOIN postings_techs_must pt1 "
+        "ON pt1.must_id=t1.id "
+    "JOIN postings_techs_must pt2 "
+        "ON pt1.posting_id=pt2.posting_id "
+    "JOIN technologies t2 "
+        "ON pt2.must_id=t2.id "
+    "WHERE t2.name=:tech "
+        "AND t1.name<>:tech "
+    "GROUP BY t1.name "
+    "ORDER BY count DESC "
+    "LIMIT 5"
+)
+
+# Get 5 technologies most often accompanying a given technology in a given city in postings
+techs_tech_city = sqlalchemy.sql.text(
+    "SELECT t1.name, t2.name, cities.name, COUNT(*) AS count "
+    "FROM technologies t1 "
+    "JOIN postings_techs_must AS pt1 "
+        "ON pt1.must_id=t1.id "
+    "JOIN postings_techs_must AS pt2 "
+        "ON pt1.posting_id=pt2.posting_id "
+    "JOIN technologies t2 "
+        "ON pt2.must_id=t2.id "
+    "JOIN postings_cities "
+        "ON pt1.posting_id=postings_cities.posting_id "
+    "JOIN cities "
+        "ON postings_cities.city_id=cities.id "
+    "WHERE t2.name=:tech "
+        "AND t1.name<>:tech "
+        "AND cities.name=:city "
+    "GROUP BY t1.name "
+    "ORDER BY count DESC "
+    "LIMIT 5"
+)
--- demitaja/views.py
+++ demitaja/views.py
@@ -1,6 +1,44 @@
+from flask import render_template
+import sqlalchemy
+from demitaja.database import db_session
+from demitaja.models import Posting, City, Technology
 from demitaja import app
+from demitaja.utils import queries
 
 
 @app.route('/')
 def home():
     return 'Hello world!'
+
+
+@app.route('/summary')
+def summary():
+    req_city = 'Warsaw'
+    req_tech = 'Django'
+    techs = db_session.execute(queries.techs).fetchall()
+    cities = db_session.execute(queries.cities).fetchall()
+    city = db_session.execute(queries.city, {'city': req_city}).fetchone()
+    if not city:
+        city = (req_city, 0)
+    tech = db_session.execute(queries.tech, {'tech': req_tech}).fetchone()
+    if not tech:
+        tech = (req_tech, 0)
+    cities_tech = db_session.execute(queries.cities_tech, {'tech': req_tech}).fetchall()
+    techs_city = db_session.execute(queries.techs_city, {'city': req_city}).fetchall()
+    tech_city = db_session.execute(queries.tech_city, {'city': req_city, 'tech': req_tech}).fetchone()
+    if not tech_city:
+        tech_city = (req_tech, req_city, 0)
+    techs_tech = db_session.execute(queries.techs_tech, {'tech': req_tech}).fetchall()
+    techs_tech_city = db_session.execute(queries.techs_tech_city, {'city': req_city, 'tech': req_tech}).fetchall()
+    return render_template('market-summary.html',
+                           req_city=req_city,
+                           req_tech=req_tech,
+                           techs=techs,
+                           cities=cities,
+                           city=city,
+                           tech=tech,
+                           cities_tech=cities_tech,
+                           techs_city=techs_city,
+                           tech_city=tech_city,
+                           techs_tech=techs_tech,
+                           techs_tech_city=techs_tech_city)
